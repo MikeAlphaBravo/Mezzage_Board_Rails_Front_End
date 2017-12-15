84bc94fbe7895418b2124461f79db572777f51cd
Guest User <Guest@Epicodus-2D.local>
2017-12-14 16:21:05 -0800

    add working show for api to rails front end

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,11 @@
 class BoardsController < ApplicationController
   def index
     @boards = AllBoards.new.get_all_boards
+    # binding.pry
+  end
+
+  def show
+    @board = AllBoards.new.get_board(params[:id])
+    binding.pry
   end
 end
--- app/services/all_boards.rb
+++ app/services/all_boards.rb
@@ -2,4 +2,8 @@ class AllBoards
   def get_all_boards
     HTTParty.get('http://localhost:3001/boards')
   end
+
+  def get_board(id)
+    HTTParty.get('http://localhost:3001/boards/' + id)
+  end
 end
